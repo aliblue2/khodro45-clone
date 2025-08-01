28a8db15ac54ede50ff7a81ca0000426b0f6cb10
alireza <alirezaabdolmaleki600@gmail.com>
2025-08-01 23:22:53 +0330

    Refactor main function to initialize configuration, logging, and database connections

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "github.com/aliblue2/khodro45/api"
+import (
+	"github.com/aliblue2/khodro45/api"
+	"github.com/aliblue2/khodro45/configs"
+	"github.com/aliblue2/khodro45/data/cache"
+	"github.com/aliblue2/khodro45/data/db"
+	"github.com/aliblue2/khodro45/pkg/logging"
+)
 
 func main() {
-	api.InitiateApp()
+	cfg := configs.GetConfig()
+	logger := logging.NewLogger(cfg)
+
+	err := cache.InitiateRedis(cfg)
+	defer cache.CloseRedis()
+
+	if err != nil {
+		logger.Fatal(logging.Redis, logging.Startup, err.Error(), nil)
+	}
+
+	err = db.InitiateDb(cfg)
+	defer db.CloseDb()
+	if err != nil {
+		logger.Fatal(logging.Postgres, logging.Startup, err.Error(), nil)
+		panic(err)
+	}
+
+	api.InitiateServer(cfg)
 }
